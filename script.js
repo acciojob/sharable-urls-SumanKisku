6f3eb26bac5d382dc7702f252784bd1afe19b1ca
Suman Kisku <sumankisku1@gmail.com>
2023-04-29 14:58:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // your code here
+const url = document.getElementById("url");
+const name = document.getElementById("name");
+const year = document.getElementById("year");
+const button = document.querySelector("button");
+
+button.addEventListener("click", (e)=> {
+	e.preventDefault();
+	url.textContent = `https://localhost:8080/`
+	if(name.value && year.value) {
+		url.textContent = `${url.textContent}?name=${name.value}&year=${year.value}`;
+	} else if(name.value) {
+		url.textContent = `${url.textContent}?name=${name.value}`;
+	} else if(year.value) {
+		url.textContent = `${url.textContent}?year=${year.value}`;
+		
+	}
+
+})
+
